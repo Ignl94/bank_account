96f4a0d8da6eec03aa256a9f005ba40d890b4305
Ignl94 <theenthusedwriter@gmail.com>
2021-01-30 14:58:05 -0500

    commit

---
+++ BankAccount.py
@@ -0,0 +1,32 @@
+import random
+
+
+def account_gen():
+    account = ""
+    total = 0
+    while True:
+        account += random.randint(0, 9)
+        total += 1
+        if total == 8:
+            return account
+
+
+class BankAccount:
+    routing_number = 109991919
+    balance = 0
+    account_number = account_gen()
+
+    def __init__(self, full_name):
+        self.name = full_name
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Amount Deposited: {amount}")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient Funds!")
+            self.balance -= 10
+        else:
+            self.balance -= amount
+            print(f"Amount Withdrawn: {amount}")
